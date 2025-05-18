fd097cef8a288856aecfa145fff218dca1fe8448
Inforable <hasri.fayadh@gmail.com>
2025-05-19 02:52:31 +0700

    feat: add cloneBoard dan movePiece method

--- src/model/Board.java
+++ src/model/Board.java
@@ -19,4 +19,48 @@ public class Board {
         this.g = 0;
         this.h = 0;
     }
+
+    // deep copy
+    public Board cloneBoard() {
+        int rows = board.length;
+        int cols = board[0].length;
+
+        char[][] newBoard = new char[rows][cols];
+        for (int i = 0; i < rows; i++) {
+            newBoard[i] = Arrays.copyOf(board[i], cols);
+        }
+
+        Map<Character, Piece> newPieces = new HashMap<>();
+        for (Map.Entry<Character, Piece> entry : pieces.entrySet()) {
+            Piece p = entry.getValue();
+            newPieces.put(entry.getKey(), new Piece(p.id, p.x, p.y, p.length, p.isHorizontal, p.isPrimary));
+        }
+        return new Board(newBoard, newPieces, exitX, exitY);
+    }
+
+    // Menggeser piece dengan id tertentu
+    public void movePiece(char id, int delta) {
+        Piece piece = pieces.get(id);
+        if (piece == null) return;
+
+        // Hapus dari posisi lama
+        if (piece.isHorizontal) {
+            for (int i = 0; i < piece.length; i++) {
+                board[piece.y][piece.x + i] = ' ';
+            }
+            // Update posisi
+            piece.x += delta;
+            for (int i = 0; i < piece.length; i++) {
+                board[piece.y][piece.x + i] = piece.id;
+            }
+        } else {
+            for (int i = 0; i < piece.length; i++) {
+                board[piece.y + i][piece.x] = ' ';
+            }
+            piece.y += delta;
+            for (int i = 0; i < piece.length; i++) {
+                board[piece.y + i][piece.x] = piece.id;
+            }
+        }
+    }
 }
