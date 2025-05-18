eb55045dd70916745fbdcb69e3a006f16eca030f
Inforable <hasri.fayadh@gmail.com>
2025-05-18 12:48:39 +0700

    feat: add position for exit (K)

--- src/model/Board.java
+++ src/model/Board.java
@@ -5,13 +5,16 @@ import java.util.*;
 public class Board {
     public char[][] board;
     public Map<Character, Piece> pieces;
+    public int exitX, exitY;
     public List<String> path; // Placeholder
     public int g, h;
 
     // Ctor
-    public Board(char[][] board, Map<Character, Piece> pieces) {
+    public Board(char[][] board, Map<Character, Piece> pieces, int exitX, int exitY) {
         this.board = board;
         this.pieces = pieces;
+        this.exitX = exitX;
+        this.exitY = exitY;
         this.path = new ArrayList<>();
         this.g = 0;
         this.h = 0;
